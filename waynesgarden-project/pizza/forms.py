1608c9fdef86dbda3c858108f9edf78eebf281fd
wayne71 <wayne.hatter1971@gmail.com>
2024-05-01 05:51:19 -0500

    Forms and Files

--- waynesgarden-project/pizza/forms.py
+++ waynesgarden-project/pizza/forms.py
@@ -11,9 +11,10 @@ from .models import Pizza, Size
     
 class PizzaForm(forms.ModelForm):
     
-    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
+    image = forms.ImageField()
     
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
         labels = {'topping1':'Topping 1', 'topping2': 'Topping 2'}
+        
--- waynesgarden-project/pizza/views.py
+++ waynesgarden-project/pizza/views.py
@@ -8,7 +8,7 @@ def home(request):
 # Create view for order page
 def order(request):
     if request.method == 'POST':
-        filled_form = PizzaForm(request.POST)
+        filled_form = PizzaForm(request.POST, request.FILES)
         if filled_form.is_valid():
             note = 'Thanks for ordering! Your %s %s and %s pizza is on its way' %(filled_form.cleaned_data['size'], filled_form.cleaned_data['topping1'], filled_form.cleaned_data['topping2'])
             new_form = PizzaForm()
