affd0e80fcde3a331ae6a5d4e6c9d5f7ffcaacb8
wayne71 <wayne.hatter1971@gmail.com>
2024-05-01 06:13:27 -0500

    formset views

--- waynesgarden-project/pizza/views.py
+++ waynesgarden-project/pizza/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import PizzaForm, MultiplePizzaForm
+from django.forms import formset_factory
 
 # Create view for home page
 def home(request):
@@ -17,3 +18,20 @@ def order(request):
     else:
         form = PizzaForm()
         return render(request, 'pizza/order.html', {'pizzaform':form, 'multiple_form':multiple_form})
+    
+def pizzas(self, request):
+    number_of_pizzas = 2
+    filled_multiple_pizza_form = MultiplePizzaForm(request.GET)
+    if MultiplePizzaForm.is_valid():
+        number_of_pizzas = filled_multiple_pizza_form.form.cleaned_data['number']
+    PizzaFormSet = formset_factory(PizzaForm, extra=number_of_pizzas)
+    formset = PizzaFormSet()
+    if request.method == 'POST':
+        filled_formset = PizzaFormSet(request.POST)
+        if filled_formset.is_valid():
+            for form in filled_formset:
+                print(form.cleaned_data['topping1'])
+            note = 'Pissas have been ordered!'
+        else:
+            note = 'Order was not created, please try again'
+        return render(request, 'pizza/pizzas.html', {'note':note, 'formset':formset})
