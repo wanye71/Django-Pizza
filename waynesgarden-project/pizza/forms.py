81d1b559999470d7e3cf570e91997e01ea39e7e7
wayne71 <wayne.hatter1971@gmail.com>
2024-05-01 05:40:43 -0500

    Advanced widgets

--- waynesgarden-project/pizza/forms.py
+++ waynesgarden-project/pizza/forms.py
@@ -1,16 +1,19 @@
 from django import forms
-from .models import Pizza
+from .models import Pizza, Size
 
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(label='Topping 1', max_length=100)
-    topping2 = forms.CharField(label='Topping 2', max_length=100)
-    choices = [('Small', 'Small'), 
-               ('Medium', 'Medium'), 
-               ('Large', 'Large')]
-    size = forms.ChoiceField(label='Size', choices=choices)
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='Topping 1', max_length=100)
+#     topping2 = forms.CharField(label='Topping 2', max_length=100)
+#     choices = [('Small', 'Small'), 
+#                ('Medium', 'Medium'), 
+#                ('Large', 'Large')]
+#     size = forms.ChoiceField(label='Size', choices=choices)
     
-# class PizzaForm(forms.ModelForm):
-#     class Meta:
-#         model = Pizza
-#         fields = ['topping1', 'topping2', 'size']
-#         labels = {'topping1':'Topping 1', 'topping2': 'Topping 2'}
+class PizzaForm(forms.ModelForm):
+    
+    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
+    
+    class Meta:
+        model = Pizza
+        fields = ['topping1', 'topping2', 'size']
+        labels = {'topping1':'Topping 1', 'topping2': 'Topping 2'}
