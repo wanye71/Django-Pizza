0dbf08f03b298d94c2a9d11a19bdb2e8e0df58eb
wayne71 <wayne.hatter1971@gmail.com>
2024-05-01 05:26:13 -0500

    Working with widgets

--- waynesgarden-project/pizza/forms.py
+++ waynesgarden-project/pizza/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from .models import Pizza
 
-# class PizzaForm(forms.Form):
-#     topping1 = forms.CharField(label='Topping 1', max_length=100)
-#     topping2 = forms.CharField(label='Topping 2', max_length=100)
-#     choices = [('Small', 'Small'), 
-#                ('Medium', 'Medium'), 
-#                ('Large', 'Large')]
-#     size = forms.ChoiceField(label='Size', choices=choices)
+class PizzaForm(forms.Form):
+    topping1 = forms.CharField(label='Topping 1', max_length=100)
+    topping2 = forms.CharField(label='Topping 2', max_length=100)
+    choices = [('Small', 'Small'), 
+               ('Medium', 'Medium'), 
+               ('Large', 'Large')]
+    size = forms.ChoiceField(label='Size', choices=choices)
     
-class PizzaForm(forms.ModelForm):
-    class Meta:
-        model = Pizza
-        fields = ['topping1', 'topping2', 'size']
-        labels = {'topping1':'Topping 1', 'topping2': 'Topping 2'}
+# class PizzaForm(forms.ModelForm):
+#     class Meta:
+#         model = Pizza
+#         fields = ['topping1', 'topping2', 'size']
+#         labels = {'topping1':'Topping 1', 'topping2': 'Topping 2'}
